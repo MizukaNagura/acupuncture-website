a1c502425db019fc9f38518b250e9216649fc8c5
sashiko <sasshi@sashikonoMacBook-Pro.local>
2023-11-01 23:06:48 +0900

    app/models/acupuncture_point.pyのコメント追加

--- app/models/acupuncture_point.py
+++ app/models/acupuncture_point.py
@@ -1,10 +1,18 @@
-from sqlalchemy import create_engine, Column, Integer, String, Sequence
+from sqlalchemy import Column, Integer, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
 
+# declarative_base() は、クラス定義を使用してSQLAlchemyテーブルを定義するための基底クラスを作成します。
 Base = declarative_base()
 
 class AcupuncturePoint(Base):
+    # __tablename__ 属性はこのモデルによってデータベース内で使用されるテーブルの名前を設定します。
     __tablename__ = "acupuncture_points"
+    
+    # 各ツボの一意の識別子であり、データベース内でプライマリキーとして機能します。
     id = Column(Integer, Sequence("acupuncture_point_id_seq"), primary_key=True)
+    
+    # ツボの名前を格納するためのカラムです。
     name = Column(String)
+    
+    # ツボの説明や効能を格納するためのカラムです。
     description = Column(String)
