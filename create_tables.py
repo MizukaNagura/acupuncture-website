0265c367a790956bc0d9acabd4982a4fdd1fd6b0
sashiko <sasshi@sashikonoMacBook-Pro.local>
2023-11-01 23:42:27 +0900

    create_tables.py修正

--- create_tables.py
+++ create_tables.py
@@ -1,7 +1,5 @@
-# create_tables.py
-# app.models.acupuncture_point の代わりに
-from database.database import engine
 from app.models.acupuncture_point import Base
+from database.database import engine
 
 # データベーステーブルを作成する
 Base.metadata.create_all(bind=engine)
