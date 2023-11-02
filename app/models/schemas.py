b4c1b654ad4dfb07f210c3d9c6a55f900b2e378e
sashiko <sasshi@sashikonoMacBook-Pro.local>
2023-11-02 18:47:11 +0900

    Pydantic スキーマの定義

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,23 +1,31 @@
 from pydantic import BaseModel
 
+# 基本的なツボ情報を表すスキーマです。これは他のスキーマのベースとして使用されます。
 class AcupuncturePointBase(BaseModel):
     name: str
     description: str
 
+# ツボを作成する際に使用するリクエストボディのスキーマです。
 class AcupuncturePointCreate(AcupuncturePointBase):
     pass
 
+# ツボを更新する際に使用するリクエストボディのスキーマです。
 class AcupuncturePointUpdate(AcupuncturePointBase):
     pass
 
+# データベースから読み取ったツボ情報をレスポンスとして返す際に使用するスキーマです。
+# idフィールドが追加されています。
 class AcupuncturePointInDBBase(AcupuncturePointBase):
     id: int
 
+    # Pydanticの設定。ORMモデルとしても使用される場合に必要です。
     class Config:
-        from_attributes = True
+        orm_mode = True
 
+# レスポンスで使用される完全なツボ情報のスキーマです。
 class AcupuncturePoint(AcupuncturePointInDBBase):
     pass
 
+# データベース内のツボ情報を表すスキーマです。
 class AcupuncturePointInDB(AcupuncturePointInDBBase):
     pass
