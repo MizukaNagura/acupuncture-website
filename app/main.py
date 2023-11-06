eeaa7903e26e87b39db3a3ae3e4653ec15a40a91
sashiko <sasshi@sashikonoMacBook-Pro.local>
2023-11-06 21:51:57 +0900

    エンドポイントの定義修正

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from .routes.acupuncture_points import router as acupuncture_points_router
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
+from .schemas import Item
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")  # templatesディレクトリを設定
@@ -13,11 +14,22 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # APIルーターのインクルード
 app.include_router(acupuncture_points_router)
 
-# ルートエンドポイント
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
-
 @app.get("/")
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/items/")
+async def read_items():
+    return {"message": "GETリクエストに対するレスポンス"}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return {"message": "POSTリクエストによってアイテムが作成されました", "item": item}
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"message": "PUTリクエストによってアイテムが更新されました", "item_id": item_id, "item": item}
+
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    return {"message": "DELETEリクエストによってアイテムが削除されました", "item_id": item_id}
--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -3,3 +3,9 @@ from pydantic import BaseModel
 class AcupuncturePointCreate(BaseModel):
     name: str
     description: str
+
+class Item(BaseModel):
+    name: str
+    description: str
+    price: float
+    tax: float = None
